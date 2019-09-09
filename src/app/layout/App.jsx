25b542f150422f45f3057f0c703eb9c4b14d4e19
Anshul889 <anshulmanaktala@gmail.com>
2019-09-09 11:55:56 +0530

    folder structure

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+        <button className='ui icon'>Lul</button>
+      </div>
+    );
+  }
+}
