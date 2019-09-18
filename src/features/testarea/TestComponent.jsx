0666aa5312829d927b44497422b6fa05813a5d96
Anshul889 <anshulmanaktala@gmail.com>
2019-09-18 16:05:07 +0530

    google maps test

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -3,6 +3,11 @@ import { connect } from 'react-redux'
 import {incrementCounter, decrementCounter} from './testActions';
 import { Button } from 'semantic-ui-react';
 import TestPlaceInput from './TestPlaceInput';
+import SimpleMap from './SimpleMap';
+import {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
 const actions = {
   incrementCounter,
@@ -10,6 +15,24 @@ const actions = {
 }
 
 export class TestComponent extends Component {
+  state = {
+    latlng: {
+      lat: 59.95,
+      lng: 30.33
+    }
+  }
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latlng: latLng 
+        })
+      })
+      .catch(error => console.error('Error', error));
+  };
+
   render() {
     const {data, incrementCounter, decrementCounter} = this.props;
     return (
@@ -20,7 +43,8 @@ export class TestComponent extends Component {
         <Button onClick={decrementCounter} negative content="Decrement"></Button>
         <br />
         <br />
-        <TestPlaceInput />
+        <TestPlaceInput selectAddress={this.handleSelect}/>
+        <SimpleMap key={this.state.latlng.lng}latlng={this.state.latlng}/>
       </div>
     )
   }
