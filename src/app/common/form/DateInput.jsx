cc9b4aff7a876c465a2241a7ba46c332ca072003
Anshul889 <anshulmanaktala@gmail.com>
2019-09-22 14:54:31 +0530

    date input firebase

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import { Form, Label } from 'semantic-ui-react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import React from "react";
+import { Form, Label } from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const DateInput = ({input: {value, onBlur, onChange, ...restInput}, width, placeholder, meta: {touched, error}, ...rest}) => {
+const DateInput = ({
+  input: { value, onBlur, onChange, ...restInput },
+  width,
+  placeholder,
+  meta: { touched, error },
+  ...rest
+}) => {
   return (
     <Form.Field error={touched && !!error} width={width}>
       <DatePicker
         {...rest}
         placeholderText={placeholder}
-        selected={value ? new Date(value) : null}
+        selected={
+          value
+            ? Object.prototype.toString.call(value) !== "[object Date]"
+              ? value.toDate()
+              : value
+            : null
+        }
         onChange={onChange}
-        onBlur={onBlur}
+        onBlur={(e, val) => onBlur(val)}
         onChangeRaw={e => e.preventDefault()}
       />
-      {touched && error && <Label basic color='red'>{error}</Label>}
+      {touched && error && (
+        <Label basic color="red">
+          {error}
+        </Label>
+      )}
     </Form.Field>
-  )
-}
+  );
+};
 
-export default DateInput
+export default DateInput;
