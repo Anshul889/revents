b5a2a67f629deada7b4adde00d804eff3a264658
Anshul889 <anshulmanaktala@gmail.com>
2019-09-29 14:00:10 +0530

    user tab queries

--- src/features/user/UserDetailed/UserDetailedEvents.jsx
+++ src/features/user/UserDetailed/UserDetailedEvents.jsx
@@ -1,21 +1,22 @@
 import React, { Fragment } from 'react'
-import { Grid, Segment, Header, Menu, Card, Image } from 'semantic-ui-react'
+import { Grid, Segment, Header, Menu, Card, Image, Tab } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 
-const UserDetailedEvents = ({events, eventsLoading}) => {
+const panes = [
+  {menuItem: 'All Events', pane: {key: 'allEvents'}},
+  {menuItem: 'Past Events', pane: {key: 'pastEvents'}},
+  {menuItem: 'Future Events', pane: {key: 'futureEvents'}},
+  {menuItem: 'Hosting', pane: {key: 'hosted'}},
+]
+
+const UserDetailedEvents = ({events, eventsLoading, changeTab}) => {
   return (
     <Fragment>
       <Grid.Column width={12}>
           <Segment attached loading={eventsLoading}>
             <Header icon='calendar' content='Events' />
-            <Menu secondary pointing>
-              <Menu.Item name='All Events' active />
-              <Menu.Item name='Past Events' />
-              <Menu.Item name='Future Events' />
-              <Menu.Item name='Events Hosted' />
-            </Menu>
-
+              <Tab onTabChange={(e, data) => changeTab(e, data)} panes={panes} menu={{secondary: true, pointing: true}}/>
             <Card.Group itemsPerRow={5}>
             {events && events.map(event => (
               <Card as={Link} to={`/event/${event.id}`} key={event.id}>
