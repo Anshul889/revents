9162ac984b5fbe826978907029ef0606ac161ab8
Anshul889 <anshulmanaktala@gmail.com>
2019-10-03 12:21:54 +0530

    home page features

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -9,7 +9,17 @@ const HomePage = ({history}) => {
             <img className="ui image massive" src="/assets/logo.png" alt="logo" />
             <div className="content">Re-vents</div>
           </h1>
-          <h2>Do whatever you want to do</h2>
+          <ul>
+            <li>Login and Register functionality using Firebase authentication</li>
+            <li>Social login for Facebook and Google into the application</li>
+            <li>Google maps and Places autocomplete integration</li>
+            <li>Photo uploading using drag and drop, with resizing and cropping of the images before upload</li>
+            <li>Live chat system</li>
+            <li>Paging, Sorting and Filtering with Firestore</li>
+            <li>Infinite scroll for the pagination</li>
+            <li>Firestore database design</li>
+            <li>Creating reusable form components with Redux forms</li>
+          </ul>
           <div onClick={() => history.push('/events')} className="ui huge white inverted button">
             Get Started
             <i className="right arrow icon" />
